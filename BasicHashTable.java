4585e204c477cb807588dc9ff467b9c32ed00591
Temmy Techie <91670087+Temmytechguy@users.noreply.github.com>
2023-01-27 15:08:54 +0100

    updated Hashtable to include delete method

--- BasicHashTable.java
+++ BasicHashTable.java
@@ -4,6 +4,7 @@ public class BasicHashTable<X,Y> {
 	
 	private HashEntry[] data;
 	private int capacity;
+	private int size;
 	
 	
 	
@@ -12,10 +13,91 @@ public class BasicHashTable<X,Y> {
 		
 		this.capacity = tableSize;
 		this.data = new HashEntry[capacity];
+		this.size = 0;
+		
+	}
+	
+
+
+
+
+	public int getSize() {
+		return this.size;
 	}
 
 
 
+
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+
+
+	public Y get(X key)
+	{
+		int hash = calculateHash(key);
+		
+		//if we dont have anything for the given key, just return null
+		if(data[hash] == null)
+		{
+			return null;
+		}
+		//otherwise get the hashentry for the key and return its value
+		else {
+			return (Y)data[hash].getValue();
+		}
+		
+	}
+	
+	public void put(X key, Y value)
+	{
+		int hash = calculateHash(key);
+		data[hash] = new HashEntry<X,Y>(key, value);
+		size++;
+	}
+	
+	
+	public Y delete(X key)
+	{
+		//first get the value for this key 
+		Y value = get(key);
+		
+		//clear out the hashtable slot for the key and return the removed value
+		if(value != null)
+		{
+			int hash = calculateHash(key);
+			data[hash] = null;
+			size--;
+			hash = (hash + 1)% this.capacity;
+			
+			//if the next slot isnt empty we should re add it so we can keep the hash algorithm clean
+			while(data[hash] != null)
+			{
+				HashEntry he = data[hash];
+				data[hash] = null;
+				put((X)he.getKey(), (Y)he.getValue());
+				//we repositioned the hash item and didnt really add a new one so back off the size
+				size--;
+				hash = (hash + 1)%this.capacity;
+			}
+		}
+		return value;
+		
+	}
+	private int calculateHash(X key)
+	{
+		int hash = (key.hashCode() % this.capacity);
+		
+		//this is necessary to deal with collision
+		while(data[hash] != null && !data[hash].getKey().equals(key))
+		{
+			hash = (hash + 1) % this.capacity;
+		}
+		return hash;
+	}
+	
 
 	private class HashEntry<X,Y>
 	{
@@ -24,7 +106,7 @@ public class BasicHashTable<X,Y> {
 		
 		
 		public HashEntry(X key, Y value) {
-			super();
+			
 			this.key = key;
 			this.value = value;
 		}
